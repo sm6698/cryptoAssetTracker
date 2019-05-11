573b7316b964ae55328eae1af9562ddc1b4eda94
sm6698 <sm6698@nyu.edu>
2019-05-11 13:56:03 -0700

    fix linting issues

--- modules/cryptoTrackerBinance.py
+++ modules/cryptoTrackerBinance.py
@@ -1,24 +1,27 @@
+from decimal import Decimal
 from shutil import copyfile
 from openpyxl import load_workbook
 from modules.cryptoTrackerUtil import *
 
+
 def bin_deposit_coin(fn, cur, con):
     # fix Binance bug that downloads file as csv
     if fn[1].split('.')[1] == 'csv':
-        copyfile(fn[2],fn[2].replace('csv', 'xlsx'))
+        copyfile(fn[2], fn[2].replace('csv', 'xlsx'))
+    wb = load_workbook(fn[2].replace('csv', 'xlsx'))
+    # initialize arrays
     coin_to_exchange = []
     coin_tx_list = {}
-    wb = load_workbook(fn[2].replace('csv', 'xlsx'))
     ws = wb['sheet1']
-    #print (ws.min_row)
-    #print (ws.max_row)
+    # print (ws.min_row)
+    # print (ws.max_row)
     for row in ws.iter_rows(min_row=2):
-        #print (row[0].value)
+        # print (row[0].value)
         date_time = str(row[0].value)
-        coin =  str(row[1].value) + "_Bin"
-        #print (coin)
-        add_column_if_not_in_table(coin,cur,con)
-        tx_id = row[5].value.replace('0x','')
+        coin = str(row[1].value) + "_Bin"
+        # print (coin)
+        add_column_if_not_in_table(coin, cur, con)
+        tx_id = row[5].value.replace('0x', '')
         qty = row[2].value
         cur.execute("INSERT INTO crypto (filename, datetime, type, %s, tx_id) VALUES (?, ?, ?, ?, ?);" % coin,
                     (fn[1], date_time, 'xfer', qty, tx_id))
@@ -26,60 +29,52 @@ def bin_deposit_coin(fn, cur, con):
 
         coin_addr = str(row[4].value)
         if "BTC" in coin:
-            #coinQty[tx_id] = Decimal(qty) * SATOSHI
             coin_tx_list[tx_id] = coinObject(Decimal(qty) * SATOSHI, fn[1], coin, coin_addr)
 
         else:
-            #coinQty[tx_id] = qty
             coin_tx_list[tx_id] = coinObject(Decimal(qty), fn[1], coin, coin_addr)
         coin_to_exchange.append(tx_id)
-        #coinType[tx_id] = coin
-        #coinAddr[tx_id] = str(row[4].value)
-        #coinFn[tx_id] = fn[1]
     return [coin_tx_list, coin_to_exchange]
 
 def bin_widthdraw_coin(fn, cur, con):
     # fix Binance bug that downloads file as csv
     if fn[1].split('.')[1] == 'csv':
-        copyfile(fn[2],fn[2].replace('csv', 'xlsx'))
+        copyfile(fn[2], fn[2].replace('csv', 'xlsx'))
+    wb = load_workbook(fn[2].replace('csv', 'xlsx'))
+    # initialize arrays
     coin_from_exchange = []
     coin_tx_list = {}
-    wb = load_workbook(fn[2].replace('csv', 'xlsx'))
     ws = wb['sheet1']
-    #print (ws.min_row)
-    #print (ws.max_row)
+    # print (ws.min_row)
+    # print (ws.max_row)
     for row in ws.iter_rows(min_row=2):
-        #print (row[0].value)
-        coin_addr = str(row[4].value).replace('0x','')
+        # print (row[0].value)
+        coin_addr = str(row[4].value).replace('0x', '')
         date_time = str(row[0].value)
         coin =  str(row[1].value) + "_Bin"
-        #print (coin)
+        # print (coin)
         add_column_if_not_in_table(coin, cur, con)
         wallet = str(row[1].value).upper() + '_' + coin_addr[0:5]
         add_column_if_not_in_table(wallet, cur, con)
-        tx_id = row[5].value.replace('0x','')
+        tx_id = row[5].value.replace('0x', '')
         qty = Decimal(row[2].value) * -1
         fee = Decimal(row[3].value) * -1
-        cur.execute("INSERT INTO crypto (filename, datetime, type, %s, %s, tx_id, fee) VALUES (?, ?, ?, ?, ?, ?,?);"
+        cur.execute("INSERT INTO crypto (filename, datetime, type, %s, %s, tx_id, fee) VALUES (?, ?, ?, ?, ?, ?, ?);"
                     % (coin, wallet), (fn[1], date_time, 'xfer', str(qty + fee), str(-qty), tx_id, str(fee)))
         con.commit()
         if "BTC" in coin:
             coin_tx_list[tx_id] = coinObject(Decimal(qty) * SATOSHI, fn[1], coin, coin_addr)
-            #coinQty[tx_id] = Decimal(qty) * SATOSHI
         else:
-            #coinQty[tx_id] = qty
             coin_tx_list[tx_id] = coinObject(Decimal(qty), fn[1], coin, coin_addr)
         coin_from_exchange.append(tx_id)
-        #coinType[tx_id] = coin
-        #coinAddr[tx_id] = str(coin_addr)
-        #coinFn[tx_id] = fn[1]
     return [coin_tx_list, coin_from_exchange]
+
 
 def bin_history(fn, cur, con):
     wb = load_workbook(fn[2])
     ws = wb['sheet1']
-    #print (ws.min_row)
-    #print (ws.max_row)
+    # print (ws.min_row)
+    # print (ws.max_row)
     previous_time = ""
     previous_date = ""
     previous_market = ""
@@ -90,14 +85,15 @@ def bin_history(fn, cur, con):
     coin_fee = ""
     for row in ws.iter_rows(min_row=2):
         date_time = str(row[0].value).split(" ")
-        #print (first_iteration)
-        #print (previous_date, date_time[0])
-        #print (previous_market, row[1].value)
+        # print (first_iteration)
+        # print (previous_date, date_time[0])
+        # print (previous_market, row[1].value)
         if first_iteration == 0 and (previous_date != date_time[0] or previous_market != row[1].value):
             add_column_if_not_in_table(coin, cur, con)
             add_column_if_not_in_table(coin_fee, cur, con)
-            cur.execute("INSERT INTO crypto (filename, datetime, type, %s, %s) VALUES (?, ?, ?, ?, ?);" % (coin, coin_fee),
-                        (str(fn[1]), str(previous_date) + " " + str(previous_time), 'trade', str(coin_qty), str(coin_fee_qty)))
+            cur.execute("INSERT INTO crypto (filename, datetime, type, %s, %s) VALUES (?, ?, ?, ?, ?);" %
+                        (coin, coin_fee), (str(fn[1]), str(previous_date) + " " + str(previous_time), 'trade',
+                        str(coin_qty), str(coin_fee_qty)))
             con.commit()
             coin_qty = 0
             coin_fee_qty = 0
@@ -108,9 +104,9 @@ def bin_history(fn, cur, con):
         amount = row[4].value
         total = row[5].value
         fee = row[6].value
-        #print ("coin.find(coin_fee)", str(coin.find(coin_fee)))
-        #print ("coin", coin)
-        #print ("coin_fee", coin_fee)
+        # print ("coin.find(coin_fee)", str(coin.find(coin_fee)))
+        # print ("coin", coin)
+        # print ("coin_fee", coin_fee)
         if coin.find(coin_fee) == 0:
             if tran_type == 'BUY':
                 coin_qty = coin_qty - Decimal(total)
